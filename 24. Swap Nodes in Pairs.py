26763b67fa825999eb94e28597794d743dd458f7
wetwc198892 <103273791@qq.com>
2020-02-22 12:50:05 -0800

    submit code

--- 24. Swap Nodes in Pairs.py
+++ 24. Swap Nodes in Pairs.py
@@ -8,11 +8,14 @@ class Solution:
     def swapPairs(self, head: ListNode) -> ListNode:
         if not head:
             return head
-        i = 1
-        cur = head
-        while cur.next:
-            if i%2 !=0:
-                cur.val,cur.next.val = cur.next.val,cur.val
-            cur = cur.next
-            i  += 1
-        return head
+        temp = ListNode(0)
+        temp.next = head
+        cur = temp
+        while cur.next and cur.next.next:
+            a = cur.next
+            b = cur.next.next
+            a.next = b.next
+            cur.next = b
+            cur.next.next = a
+            cur = cur.next.next
+        return temp.next
