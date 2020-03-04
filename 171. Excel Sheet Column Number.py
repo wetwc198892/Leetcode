f9a4c0495f80117d33190167fc0a96f76fdcd01b
wetwc198892 <103273791@qq.com>
2020-03-03 20:44:07 -0800

    submit code

--- 171. Excel Sheet Column Number.py
+++ 171. Excel Sheet Column Number.py
@@ -1,11 +1,7 @@
 class Solution:
     def titleToNumber(self, s: str) -> int:
         n = len(s)
-        s = s[::-1]
         result = 0
         for i in range(n):
-            if i == 0:
-                result += ord(s[i])-64
-            else:
-                result += (ord(s[i])-64)*(26**i)
+            result += (ord(s[i])-64)*(26**(n-i-1))
         return result
